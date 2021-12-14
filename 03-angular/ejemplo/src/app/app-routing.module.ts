a1c24eaf619ac5c768d44bffadf03283e7d9d596
Bryan <bryan.flores04@epn.edu.ec>
2021-12-14 15:43:36 -0500

    Rutas

--- 03-angular/ejemplo/src/app/app-routing.module.ts
+++ 03-angular/ejemplo/src/app/app-routing.module.ts
@@ -1,12 +1,64 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
+import {RutaForbiddenComponent} from "./rutas/ruta-forbidden/ruta-forbidden.component";
+import {RutaNotFoundComponent} from "./rutas/ruta-not-found/ruta-not-found.component";
+import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
+import {RutaAppComponent} from "./rutas/ruta-app/ruta-app.component";
+import {RutaUsuarioComponent} from "./rutas/ruta-usuario/ruta-usuario.component";
+import {RutaPostComponent} from "./rutas/ruta-post/ruta-post.component";
+
+//login
+//inicio
+//app
+    //usuario
+    //post usuario
+
 
 const routes: Routes = [
+
+  {
+    path:'inicio',
+    component: RutaInicioComponent,
+  },
+  {
+    path:'app',
+    component: RutaAppComponent,
+    children: [
+      {
+        path:'usuario',
+        component: RutaUsuarioComponent,
+      },
+      {
+        path:'post',
+        component: RutaPostComponent,
+      }
+    ]
+  },
   {
     path:'login',
     component: RutaLoginComponent,
+  },
+
+  {
+    path:'forbidden',
+    component: RutaForbiddenComponent,
+  },
+  {
+    path:'not-found',
+    component: RutaNotFoundComponent,
+  },
+  {
+    path:'',
+    redirectTo:'/login',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component: RutaNotFoundComponent
   }
+
+
 ];
 
 @NgModule({
