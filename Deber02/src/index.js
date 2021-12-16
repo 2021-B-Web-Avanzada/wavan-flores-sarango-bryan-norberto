62efc8f5c42568687802cb8fa13bd03a1817bd45
Bryan <bryan.flores04@epn.edu.ec>
2021-12-16 00:47:16 -0500

    Deber 1 y 2

---
+++ Deber02/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+//ejecuto el framework
+const app = express();
+//Middle ware es una funcion que procesa datos antes de nuestro servidor
+const morgan = require('morgan');
+
+//settings
+app.set('port',process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+app.use('/api/mascota',require('./routes/mascota'));
+app.use('/api/raza',require('./routes/raza'));
+
+//Cuando inicies quiero mensaje por consola server
+//starting the server
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port ${3000}');
+});
---
+++ Deber02/src/routes/mascota.js
@@ -0,0 +1,97 @@
+const {Router} = require('express');
+const router = Router();
+const _ = require('underscore');
+let mascotas = require('../datos.json');
+const {stringify} = require("nodemon/lib/utils");
+const fs = require('fs');
+
+//LEER
+router.get('/',(req,res)=>{
+    res.json(mascotas);
+});
+
+//INSERTAR
+router.post('/:idParent',(req,res)=>{
+    const {idParent}=req.params;
+    const {nombreChildren,edad,numeroVacunas,dueno,residenciaDueno} = req.body;
+    if(nombreChildren&&edad&&numeroVacunas&&dueno&&residenciaDueno){
+        const idChildren= mascotas.length+1;
+        let newMascota ={
+            idChildren,
+            nombreChildren,
+            edad,
+            numeroVacunas,
+            dueno,
+            residenciaDueno
+        }
+        for(let i=0;i<mascotas.length;i++){
+            if(mascotas[i].id==idParent){
+                mascotas[i].children.push(newMascota);
+                const json_mascota= JSON.stringify(mascotas);
+                fs.writeFileSync('src/datos.json',json_mascota,'utf-8');
+                break;
+            }
+        }
+        res.json(mascotas);
+        //res.json('guardado');
+    }else{
+        res.status(400).json({error:'Las entradas no pueden estar vacias'});
+    }
+
+});
+
+//BORRAR
+router.delete('/:idParent&:idChildren',(req,res)=>{
+    const idParent=parseInt(req.params['idParent']);
+    const idChildren=parseInt(req.params['idChildren']);
+
+
+    for(let i=0;i<mascotas.length;i++){
+
+        if(mascotas[i].id==idParent){
+            for(let j=0;j<mascotas[i]["children"].length;j++){
+                if(mascotas[i].children[j].idChildren==idChildren){
+                    mascotas[i].children=mascotas[i].children.filter(mascota=>mascota.idChildren!=idChildren);
+                    const json_mascota= JSON.stringify(mascotas);
+                    fs.writeFileSync('src/datos.json',json_mascota,'utf-8');
+                    res.send('borrado');
+                }
+            }
+        }
+
+
+    }
+
+});
+
+//ACTUALIZAR
+router.put('/:idParent&:idChildren',(req,res)=>{
+    const idParent=parseInt(req.params['idParent']);
+    const idChildren=parseInt(req.params['idChildren']);
+    const {nombreChildren,edad,numeroVacunas,dueno,residenciaDueno} = req.body;
+    if(nombreChildren&&edad&&numeroVacunas&&dueno&&residenciaDueno) {
+        let updateMascota = {
+            idChildren,
+            nombreChildren,
+            edad,
+            numeroVacunas,
+            dueno,
+            residenciaDueno
+        }
+        for(let i=0;i<mascotas.length;i++){
+            if(mascotas[i].id==idParent){
+                for(let j=0;j<mascotas[i]["children"].length;j++){
+                    if(mascotas[i].children[j].idChildren==idChildren){
+                        mascotas[i].children[j]=updateMascota;
+                        const json_mascota= JSON.stringify(mascotas);
+                        fs.writeFileSync('src/datos.json',json_mascota,'utf-8');
+                        res.send('Actualizado');
+                    }
+                }
+            }
+        }
+    }
+
+});
+
+module.exports =router;
